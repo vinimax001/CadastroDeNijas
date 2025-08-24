e006e731293d6cf0cce76d3ed0002ce2d57d4197
vinimax <vini.ciusang@gmail.com>
2025-08-24 16:30:30 -0300

    :fix: fixes minor typos in the code

--- src/main/java/dev/java10/CadastroDeNinjas/NinjaController.java
+++ src/main/java/dev/java10/CadastroDeNinjas/NinjaController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
-public class Controller {
+public class NinjaController {
 
     @GetMapping("/boasvindas")
     public String boasVindas() {
